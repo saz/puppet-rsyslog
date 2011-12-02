52001b9529c4a751374e14e53888c9d6840767df
Saz <me@saz.sh>
2011-12-02 17:04:17 +0100

    add option for logging on a client all or auth log only, alternate config template

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,9 +1,12 @@
-class rsyslog::client inherits rsyslog {
+class rsyslog::client inherits rsyslog ($log_local = false, $log_auth_local = false, $custom_config = undef) {
 	file { $rsyslog::params::client_conf:
 		owner 	=> root,
 		group 	=> root,
 		ensure 	=> file,
-		content	=> template("${module_name}/client.conf.erb"),
+        content => $custom_config ? {
+            ''      => template("${module_name}/client.conf.erb"),
+            default => template($custom_config),
+        },
 		require => Class['rsyslog::install'],
 		notify 	=> Class['rsyslog::service'],
 	}
