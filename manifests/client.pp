00a54910dcab4a886deeb7a621d75610e1c5e43a
Saz <me@saz.sh>
2011-12-03 15:21:15 +0100

    require class rsyslog::config, change ensure from file to present

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,13 +1,13 @@
-class rsyslog::client inherits rsyslog ($log_local = false, $log_auth_local = false, $custom_config = undef) {
+class rsyslog::client ($log_local = false, $log_auth_local = false, $custom_config = undef, $server = 'log') inherits rsyslog {
 	file { $rsyslog::params::client_conf:
+		ensure 	=> present,
 		owner 	=> root,
 		group 	=> root,
-		ensure 	=> file,
         content => $custom_config ? {
             ''      => template("${module_name}/client.conf.erb"),
             default => template($custom_config),
         },
-		require => Class['rsyslog::install'],
+		require => Class['rsyslog::config'],
 		notify 	=> Class['rsyslog::service'],
 	}
 }
