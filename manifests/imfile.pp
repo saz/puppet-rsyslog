4d3778998ef34a49b485f8f0d0a11daf1c06a1b7
Steffen Zieger <me@saz.sh>
2012-03-12 09:42:20 +0100

    add definition for imfile configuration

---
+++ manifests/imfile.pp
@@ -0,0 +1,19 @@
+define rsyslog::imfile(
+  $file_name,
+  $file_tag,
+  $file_facility,
+  $polling_interval = 10,
+  $file_severity = 'notice',
+  $run_file_monitor = true
+) {
+  include rsyslog::params
+
+  file { "${rsyslog::params::rsyslog_d}${name}.conf":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    contet  => content('rsyslog/imfile.erb'),
+    require => Class['rsyslog::install'],
+    notify  => Class['rsyslog::service'],
+  }
+}
