91c16aad6f41b4e0482ae83065d4b3ae95d6fef4
Steffen Zieger <me@saz.sh>
2012-03-12 09:44:30 +0100

    style-guide changes, fail on unsupported platform

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,27 +1,30 @@
 class rsyslog::params {
-    case $::operatingsystem {
-        /(Ubuntu|Debian)/: {
-            $rsyslog_package_name = 'rsyslog'
-            $relp_package_name = 'rsyslog-relp'
-            $rsyslog_d = '/etc/rsyslog.d/'
-            $rsyslog_conf = '/etc/rsyslog.conf'
-            $rsyslog_default = '/etc/default/rsyslog'
-            $spool_dir = '/var/spool/rsyslog/'
-            $service_name = 'rsyslog'
-            $client_conf = "${rsyslog_d}client.conf"
-            $server_conf = "${rsyslog_d}server.conf"
-        }
+  case $::operatingsystem {
+    ubuntu, debian: {
+      $rsyslog_package_name = 'rsyslog'
+      $relp_package_name = 'rsyslog-relp'
+      $rsyslog_d = '/etc/rsyslog.d/'
+      $rsyslog_conf = '/etc/rsyslog.conf'
+      $rsyslog_default = '/etc/default/rsyslog'
+      $spool_dir = '/var/spool/rsyslog/'
+      $service_name = 'rsyslog'
+      $client_conf = "${rsyslog_d}client.conf"
+      $server_conf = "${rsyslog_d}server.conf"
     }
+    default: {
+      fail("Unsupported platform: ${::operatingsystem}")
+    }
+  }
 
-    #
-    # Internal variables
-    #
+  #
+  # Internal variables
+  #
 
-    # Drop privileges to this user and group
-    $run_user = 'root'
-    $run_group = 'root'
+  # Drop privileges to this user and group
+  $run_user = 'root'
+  $run_group = 'root'
 
-    # User and group of log files
-    $log_user = 'root'
-    $log_group = 'adm'
+  # User and group of log files
+  $log_user = 'root'
+  $log_group = 'adm'
 }
