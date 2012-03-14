cf9d8d113de24031bc8ef28c8f45c946325f89ad
Will Hopper <whopper@cat.pdx.edu>
2012-03-13 21:56:29 -0700

    Adding support for FreeBSD

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,9 @@
 class rsyslog::install {
     package { $rsyslog::params::rsyslog_package_name:
-        ensure => latest,
+        ensure => $rsyslog::params::package_status,
     }
 
     package { $rsyslog::params::relp_package_name:
-        ensure => latest,
+        ensure => $rsyslog::params::package_status
     }
 }
