3b49f0c389e04971e1a3623e731e588b7fadb2d1
Yasser Nabi <yassersaleemi@gmail.com>
2014-07-07 13:30:14 +0100

    Add parameter 'v8plus' for rsyslog version 8+
    There has been some significant config changes in versions 8+ of
    Rsyslog, one of which is causing deprecated warnings with the Puppet
    module. This commit adds are class boolean parameter 'v8plus' to signify
    if the version of Rsyslog is version 8 or greater.
    
    Also, the deprecation warning is fixed (correct syntax is used).

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,7 +37,8 @@ class rsyslog (
   $server_conf            = $rsyslog::params::server_conf,
   $ssl                    = $rsyslog::params::ssl,
   $modules                = $rsyslog::params::modules,
-  $preserve_fqdn          = $rsyslog::params::preserve_fqdn
+  $preserve_fqdn          = $rsyslog::params::preserve_fqdn,
+  $v8plus                 = $rsyslog::params::v8plus
 ) inherits rsyslog::params {
   class { 'rsyslog::install': }
   class { 'rsyslog::config': }
