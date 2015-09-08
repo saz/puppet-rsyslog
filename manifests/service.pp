48877c0aa4e15f9129d4805f07d3248e0eaad3ed
Evgeniy Dementyev <e_dementyev@wargaming.net>
2015-09-08 16:53:36 +0300

    Added Solaris support

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,6 +11,14 @@
 #  class { 'rsyslog::service': }
 #
 class rsyslog::service {
+  if $::operatingsystem == "Solaris" {
+    service { 'svc:/system/system-log:default':
+      ensure  => stopped,
+      enable  => false,
+      before  => Service[$rsyslog::service_name],
+      require => Package[$rsyslog::rsyslog_package_name],
+    }
+  }
   service { $rsyslog::service_name:
     ensure     => running,
     enable     => true,
