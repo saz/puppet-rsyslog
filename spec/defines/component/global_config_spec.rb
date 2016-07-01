b9864e0bbb06f517d7ff8bf6477e60299685a1d9
Mohan Balasundaram <mbalasundaram@skyscapecloud.com>
2016-07-01 15:14:50 +0100

    fixed the global params config styling

--- spec/defines/component/global_config_spec.rb
+++ spec/defines/component/global_config_spec.rb
@@ -4,21 +4,20 @@ require 'yaml'
 describe 'rsyslog::component::global_config' do
   let(:title) { 'configoption' }
 
-  context 'when configuring a rainerscript value' do
+  context 'when configuring a legacy type value' do
     let(:params) { {
+      :type     => "legacy",
       :value    => "on",
       :priority => 40,
       :target   => '50_rsyslog.conf',
+
     }}
 
     it do
       is_expected.to contain_concat__fragment('rsyslog::component::global_config::configoption').with_content(
-        /(?x)\s*global\s*\(\n
-        \s+configoption="on"\s*\n
-        \s*\)\s*/)
+        /(?x)\$configoption\s+on\s*\n/)
     end
   end
 
 end
-    
 
